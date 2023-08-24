fbddec823a8e18baffd554777aac574e64abab37
muthafuka X <muthafuka21@gmail.com>
2023-08-24 15:53:36 +0000

    AfterLife: V1.0 Changelogs
    Signed-off-by: muthafuka X <muthafuka21@gmail.com>

---
+++ AfterLife-Changelogs.mk
@@ -0,0 +1,61 @@
+==============================================
+AfterLife Project v1.0 (Temptation) Changelogs
+==============================================
+
+- Initial Beta Release.
+- Merged august patch 13_r67 tag.
+- Switched to Version codename "Temptation".
+- Added Support for Vanilla and Gapps variants.
+- Added Advance reboot feature.
+- Added Gameprops without controller
+- Seperate tile is added for data and wifi.
+- Added three finger screenshots option.
+- Added Our Own wallpaper by default.
+- Set Own ringtone(Alarm,notification and ringtone)
+
+Settings:
+
+- Introduce new Look of Settings UI (credit AfterLife Team).	
+- Include Our Own hub "AfterLabs" designed by AfterLife Team.
+- Added support for Live Display.
+- added support for Parallel Space.
+- added support for Face Unlock.
+- Implemented GameSpace support.
+- Added UserCard.
+- Switch settings toolbar to xdroid-oss layout.
+- Added a Toggle to force LTE_CA.
+- Added AfterLife banner to firmware version.
+- Added DeepSleep preference
+- Added Wifi & Bluetooth timeout feature.
+- Move AOSP gesture to AfterLabs.
+- Redesign battery and storage bar like Oos.
+- Redesign preference category (credit to corvusOs)
+
+AfterLabs:
+- Introduce About Project (man behind this project)
+- Added battery customization.
+- Added volume rocker wake.
+- Added ambient wake gesture.
+- Added DT2W lockscreen. 
+- Added wake on plug.
+- Added media cover art.
+- Added photo spoof
+- Added Click to partial screenshot.
+- Added Ignore window secure flags.
+- Added Colored notification icon.
+- Added Feature reTicker.
+- Added Less boring HeadUp.
+- Added Brightness slider position.
+- Added Qs tile animation.
+- Added Clock customization.
+- Added Network traffic monitor.
+- Added Show 4G instead LTE.
+- Added Old style mobile data icon.
+- Added Show data disable icon.
+- Added Monet Customization.
+- Added Font picker support.
+- Added Black Theme.
+- Added Immersive Navbar.
+- Bring back Icon pack and signal wifi icon from afterlife 12.
+
+Note: This is beta version,remember.
